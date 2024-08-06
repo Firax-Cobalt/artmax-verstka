c6320fb3fe13560b41f4f5f255fd0187c4ea2d8e
Firax_Cobalt <123969490+Firax-Cobalt@users.noreply.github.com>
2024-08-06 16:14:18 +0500

    добавил события кнопкам "Открыть" и "Закрыть" для сайдбара

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+function header__button_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function header__button_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}

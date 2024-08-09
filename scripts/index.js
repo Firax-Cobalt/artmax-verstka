b033f1e4cae2bc9865dad1c6f427c6c42c95df3e
Firax_Cobalt <123969490+Firax-Cobalt@users.noreply.github.com>
2024-08-09 11:02:35 +0500

    Добавил функцию js - при прокрутке вниз, появляется кнопка, без анимаций

--- scripts/index.js
+++ scripts/index.js
@@ -14,23 +14,32 @@ function header__button_close() {
   document.body.style.overflow = ""; // Разрешить прокрутку
 }
 
-function serviceServices(evt, serviceName) {
-  // Declare all variables
-  var i, tabcontent, tablinks;
-
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
+window.onscroll = function() {
+  if(window.scrollY>400){
+    document.getElementById('header-button').style.display = "flex";
   }
-
-  // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  else {
+     document.getElementById('header-button').style.display = "none";
   }
-
-  // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(serviceName).style.display = "flex";
-  evt.currentTarget.className += " active";
 }
+
+// function serviceServices(evt, serviceName) {
+//   // Declare all variables
+//   var i, tabcontent, tablinks;
+
+//   // Get all elements with class="tabcontent" and hide them
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+
+//   // Get all elements with class="tablinks" and remove the class "active"
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+
+//   // Show the current tab, and add an "active" class to the button that opened the tab
+//   document.getElementById(serviceName).style.display = "flex";
+//   evt.currentTarget.className += " active";
+// }
